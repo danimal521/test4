e57596fb3419b5b3ffbabfb3e015f7cb0a1f76c9
Danimal <dbiscup@microsoft.com>
2024-06-10 14:25:47 -0500

    Initial checkin

---
+++ Temp.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class Temp
+    {
+        private readonly ILogger<Temp> _logger;
+
+        public Temp(ILogger<Temp> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Temp")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("100f");
+        }
+    }
+}
